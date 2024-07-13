30a59362876176d2b9227742f49c96703af9929d
kodakar <kodakarar@gmail.com>
2024-07-13 16:39:14 +0900

    add user.ts

---
+++ backend/src/models/User.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const userSchema: Schema = new Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, { timestamps: true });
+
+export default mongoose.model<IUser>('User', userSchema);
